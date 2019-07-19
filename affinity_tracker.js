3e97ee38c01b7eda19f5995bbe7b73c6ffcf0173
unknown <odionedwards@gmail.com>
2019-07-18 20:50:02 -0400

    adding event listener to submit button for adding items to the cart

--- affinity_tracker.js
+++ affinity_tracker.js
@@ -3,3 +3,16 @@ alert("test_script.js");
 if (window.utag_data)
   alert(`The product category is ${window.utag_data.product_category}`);
 else alert("Failed to access gloable Variable!");
+
+//everytime the user visits new page
+
+//fire event everytime the user adds an item to the cart
+const cartButton = document.querySelector(".c-product-add-to-cart__button");
+
+if (cartButton) {
+  let count = 0;
+  cartButton.addEventListener("click", () => {
+    count++;
+    console.log(`added to cart: ${count}`);
+  });
+}
